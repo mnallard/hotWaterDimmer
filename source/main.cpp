6e0034343df7692eb368d6b0c0378c54382305a7
mnallard <martin.nallard@gmail.com>
2025-08-21 14:34:06 +0200

    Create main.cpp

---
+++ source/main.cpp
@@ -0,0 +1,99 @@
+#include <Arduino.h>
+#include <ESPAsyncWebServer.h>
+#include <SPIFFS.h>
+
+const char *ssid = "tintinHome-2-4G";
+const char *password = "3916drallan16";
+
+const int led = 2;
+const int capteurLuminosite = 34;
+
+AsyncWebServer server(80);
+
+void setup()
+{
+  //----------------------------------------------------Serial
+  Serial.begin(115200);
+  Serial.println("\n");
+
+  //----------------------------------------------------GPIO
+  pinMode(led, OUTPUT);
+  digitalWrite(led, LOW);
+  pinMode(capteurLuminosite, INPUT);
+
+  //----------------------------------------------------SPIFFS
+  if(!SPIFFS.begin())
+  {
+    Serial.println("Erreur SPIFFS...");
+    return;
+  }
+
+  File root = SPIFFS.open("/");
+  File file = root.openNextFile();
+
+  while(file)
+  {
+    Serial.print("File: ");
+    Serial.println(file.name());
+    file.close();
+    file = root.openNextFile();
+  }
+
+  //----------------------------------------------------WIFI
+  WiFi.begin(ssid, password);
+	Serial.print("Tentative de connexion...");
+	
+	while(WiFi.status() != WL_CONNECTED)
+	{
+		Serial.print(".");
+		delay(100);
+	}
+	
+	Serial.println("\n");
+	Serial.println("Connexion etablie!");
+	Serial.print("Adresse IP: ");
+	Serial.println(WiFi.localIP());
+
+  //----------------------------------------------------SERVER
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+  {
+    request->send(SPIFFS, "/index.html", "text/html");
+  });
+
+  server.on("/w3.css", HTTP_GET, [](AsyncWebServerRequest *request)
+  {
+    request->send(SPIFFS, "/w3.css", "text/css");
+  });
+
+  server.on("/script.js", HTTP_GET, [](AsyncWebServerRequest *request)
+  {
+    request->send(SPIFFS, "/script.js", "text/javascript");
+  });
+
+  server.on("/getHotWaterValue", HTTP_GET, [](AsyncWebServerRequest *request)
+  {
+    int val = 20;
+    String luminosite = String(val);
+    request->send(200, "text/plain", luminosite);
+  });
+
+  server.on("/on", HTTP_GET, [](AsyncWebServerRequest *request)
+  {
+    digitalWrite(led, HIGH);
+    request->send(200);
+  });
+
+  server.on("/off", HTTP_GET, [](AsyncWebServerRequest *request)
+  {
+    digitalWrite(led, LOW);
+    request->send(200);
+  });
+
+  server.begin();
+  Serial.println("Serveur actif!");
+}
+
+void loop()
+{
+
+}
